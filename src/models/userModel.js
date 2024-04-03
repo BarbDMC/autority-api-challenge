914348446df33827f59d0f0cb4df1b803002229c
BarbDMC <barbaradmorantesc@gmail.com>
2024-04-02 22:36:32 -0500

    refactor(models): Remove generate token function from model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,16 +1,9 @@
 import { DataTypes, Model } from 'sequelize';
 
-import { generateToken } from '@/services/token';
-
 export default function (sequelize) {
   class User extends Model {
     get fullName() {
       return `${this.firstName} ${this.lastName}`;
-    }
-
-    generateToken(expiresIn = '1h') {
-      const data = { id: this.id, email: this.email };
-      return generateToken(data, expiresIn);
     }
   }
 
